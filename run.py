401f4ed9209bf712b0c0404c94b9b7edb1b38ce6
Rich Ricot <richricot@gmail.com>
2024-03-03 09:58:12 +0000

    Add valiudaton to check for 6 values in provided data.

--- run.py
+++ run.py
@@ -24,7 +24,24 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"the data produced is {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Converts strings to integers.
+    Raises ValueError
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 get_sales_data()
+
+
 
